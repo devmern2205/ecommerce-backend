1ec534b2cba9a2cd034ed3c4ce52d726c43eb5ad
devmern2205 <citmern2205@gmail.com>
2024-01-24 17:43:22 +0600

    day5

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,4 +1,5 @@
 const CategoryList = require("../models/categorySchema");
+const subCategoryList = require("../models/subCategorySchema");
 
 async function createCategoryController(req, res){
   const {name,description} = req.body;
@@ -34,5 +35,62 @@ async function categoryStatusController(req,res){
   res.json({success: "Status updated"})
 }
 
+// SUB CategoryList
 
-module.exports = {createCategoryController, categoryStatusController};
+
+async function createSubCategoryController(req, res){
+  const {name,description,category} = req.body;
+  const duplicateCategory = await subCategoryList.find({name});
+  if(duplicateCategory.length > 0  ){
+    res.json({error: 'This category already exists, try again'})
+  }
+  const subCategory = new subCategoryList({
+    name,
+    description,
+    category
+  });
+  console.log(subCategory._id);
+  // console.log(subCategory.category);
+  await CategoryList.findOneAndUpdate(
+    {_id: subCategory.category},
+    {$push: {subCategory: subCategory._id}},
+    {new: true}
+  )
+  res.json({success: 'Sub Category create successfully'})
+  subCategory.save();
+}
+
+
+async function subCategoryStatusController(req,res){
+  const {name,status} = req.body;
+ 
+
+  if(status == 'rejected' || status == "waiting"){
+    const updatecategory = await subCategoryList.findOneAndUpdate(
+      {name},
+      {$set: {isActive: false, status: status}},
+      {new: true}
+    )
+  }else if(status == 'approved'){
+    const updatecategory = await subCategoryList.findOneAndUpdate(
+      {name},
+      {$set: {isActive: true, status: status}},
+      {new: true}
+    )
+  }
+  res.json({success: "Status updated"})
+}
+
+
+// get all category
+
+async function getAllCategory(req,res){
+  const data = await CategoryList.find({}).populate("subCategory")
+  res.json(data)
+}
+async function getSubAllCategory(req,res){
+  const data = await subCategoryList.find({})
+  res.json(data)
+}
+
+module.exports = {createCategoryController, categoryStatusController, createSubCategoryController, subCategoryStatusController, getAllCategory,getSubAllCategory};
--- route/api/category.js
+++ route/api/category.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const {createCategoryController,categoryStatusController} = require('../../controller/categoryController');
+const {createCategoryController,categoryStatusController, createSubCategoryController, subCategoryStatusController, getAllCategory, getSubAllCategory} = require('../../controller/categoryController');
 const router = express.Router();
 
 
  router.post('/createcategory', createCategoryController);
  router.post('/statuscategory', categoryStatusController);
+ router.post('/createsubcategory', createSubCategoryController);
+ router.post('/statussubcategory', subCategoryStatusController);
+router.get('/getallcategory', getAllCategory)
+router.get('/getallsubcategory', getSubAllCategory)
 module.exports = router;
