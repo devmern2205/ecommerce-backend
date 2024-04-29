cd0da591a6b3c9493257ac1c8e9013b736b379a2
devmern2205 <citmern2205@gmail.com>
2024-04-29 17:31:41 +0600

    user

--- route/api/authentication.js
+++ route/api/authentication.js
@@ -3,11 +3,13 @@ const registrationController = require('../../controller/registrationController'
 const emailVerificationController = require('../../controller/emailVerificationController');
 const loginController = require('../../controller/loginController');
 const verificationController = require('../../controller/verificationController');
+const userDetailsController = require('../../controller/userDetailsController');
 const router = express.Router();
 
 router.post('/registration', registrationController)
 router.post('/verification', emailVerificationController)
 router.post('/login', loginController)
 router.get('/emailverification/:id', verificationController)
+router.get('/allusers', userDetailsController)
 
 module.exports = router;
